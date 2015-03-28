71be4af1bb925f0e9bdfb7c597594ba22fbbf031
pointybeard <hi@alistairkearney.com>
2015-03-28 11:02:36 +1000

    Added more unit tests. 100% code coverage.

--- tests/ArgumentsTest.php
+++ tests/ArgumentsTest.php
@@ -8,16 +8,25 @@ class CommandTest extends \PHPUnit_Framework_TestCase
     public function testValidPassArgsToConstructor()
     {
         $it = new ArgumentIterator(false, [
-            '../blah/blah',
             '--hithere',
             '-i',
-            '-c',
-            'cheese'
+            '-c cheese',
+            '--database myDB',
+            '-h:local:host'
         ]);
 
-        $this->assertEquals(4, iterator_count($it));
-        $this->assertEquals('cheese', $it->find('c')->value);
+        $this->assertEquals(5, iterator_count($it));
         $this->assertTrue($it->find('hithere')->value);
+        $this->assertEquals('cheese', $it->find('c')->value);
+
+        // Since iterator_count() was used, the iterator position should be at the end.
+        $this->assertEquals(5, $it->key());
+
+        // Return to start and check the first item is "hithere" with a value of true
+        $it->rewind();
+        $this->assertEquals(0, $it->key());
+        $this->assertEquals('hithere', $it->current()->name);
+        $this->assertTrue($it->current()->value);
     }
     
     // Set the "ignoreFirst" property to true and check that
@@ -30,5 +39,32 @@ class CommandTest extends \PHPUnit_Framework_TestCase
         ]);
         $this->assertEquals(1, iterator_count($it));
         $this->assertFalse($it->find('../blah/blah'));
+    }
+
+    // Seed the global $argv array with some data
+    public function testValidARGV()
+    {
+        // Seed the $argv array
+        global $argv;
+        $argv = [
+            '../blah/blah',
+            '--hithere',
+            '-i',
+            '-c',
+            'cheese',
+            '-p:\Users\pointybeard\Sites\shellargs\\'
+        ];
+
+        $it = new ArgumentIterator();
+        $this->assertEquals(4, iterator_count($it));
+        $this->assertTrue($it->find('hithere')->value);
+        $this->assertEquals('\Users\pointybeard\Sites\shellargs\\', $it->find('p')->value);
+    }
+
+    // Give a argument string to the constructor
+    public function testValidArgString()
+    {
+        $it = new ArgumentIterator(false, ['--hithere -i -c cheese']);
+        $this->assertEquals(3, iterator_count($it));
     }
 }
