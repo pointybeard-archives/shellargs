9d60d418c2ddd5ec1f6d0a49dbae73967e98cb63
pointybeard <hi@alistairkearney.com>
2015-06-02 21:08:41 +1000

    __get() now throws E_USER_DEPRECATED. Instead, use name() and value() getter methods.

--- src/Lib/Argument.php
+++ src/Lib/Argument.php
@@ -1,7 +1,7 @@
 <?php
 namespace pointybeard\ShellArgs\Lib;
 
-Class Argument
+class Argument
 {
     private $name;
     private $value;
@@ -11,10 +11,42 @@ Class Argument
         $this->name = $name;
         $this->value = $value;
     }
-
+    /**
+     * Getter magic method for the $name and $value properties
+     * DO NOT USE. THIS IS DEPRECATED AND WILL BE REMOVE IN FUTURE VERSIONS
+     * Use $this->name() and $this->value() instead.
+     *
+     * @param  string       $name
+     * @return string|false
+     */
     public function __get($name)
     {
-        if($name != "name" && $name != "value"){ return false; }
+        trigger_error('Magic method __get() will no longer be used as getter for $name and $value. Please use the name() and value()  methods instead.', E_USER_DEPRECATED);
+
+        if ($name != "name" && $name != "value") {
+            return false;
+        }
+
         return $this->$name;
+    }
+
+    /**
+     * Returns the $name property
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Returns the $value property
+     *
+     * @return string
+     */
+    public function value()
+    {
+        return $this->value;
     }
 }
